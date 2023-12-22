883d9bd0b6ee1ecd2b045217d29f3248511ed929
Mustafa Furkan Tekin <mustafafurkantekin@gmail.com>
2023-12-22 15:42:48 +0300

    Update main.jsx

--- Hafta6/Conversion-App/src/main.jsx
+++ Hafta6/Conversion-App/src/main.jsx
@@ -9,8 +9,5 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppOdev />
-    {/* <AppUc /> */}
-    {/* <AppIki /> */}
-    {/* <App /> */}
   </React.StrictMode>,
 )
